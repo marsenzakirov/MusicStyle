344a72ad23416bc1530631a4cec2952aaa8b6cb1
marsen <marsenzakirov11@gmail.com>
2022-12-07 16:33:12 +0300

    feat: init Content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,12 @@
+export default function Content() {
+  return (
+    <div
+      style={{
+        background:
+          "linear-gradient(203.65deg, rgba(65, 14, 105, 0.89) 20.64%, rgba(17, 12, 48, 0.89) 84.77%)",
+      }}
+      className="w-full"
+    >
+    </div>
+  );
+}
