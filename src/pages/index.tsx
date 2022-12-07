60b72b32426b4edf9f976e31a9fc3fc441502807
marsen <marsenzakirov11@gmail.com>
2022-12-07 16:33:28 +0300

    feat: add components to index.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import NavBar from "@components/NavBar";
+import Content from "@components/Content";
+
 function App() {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        background: "linear-gradient(251.51deg, #180060 0%, #313373 100%)",
+      }}
+      className="flex"
+    >
+      <NavBar />
+      <Content />
+    </div>
+  );
 }
 
 export default App;
